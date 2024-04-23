79974345ab23cd68e3c6694c187276d87d14ae91
SnehaKoirala <345788@via.dk>
2024-04-23 10:50:57 +0200

    mine miner transporter

---
+++ src/producer/Miner.java
@@ -0,0 +1,15 @@
+package producer;
+
+import adapterqueue.Deposit;
+import valuable.Mine;
+
+public class Miner implements Runnable {
+    private Deposit deposit;
+    private Mine mine;
+
+
+    @Override
+    public void run() {
+
+    }
+}
